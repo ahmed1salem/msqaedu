965207e7b43ebb1cf9bde2902dc2cb97757eb0df
Ahmed Salem Khalifa <asalemkhalifa@gmail.com>
2025-06-01 23:11:14 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcfSDvxdFmBS-mCEupGxQU1V4RwHF9Vjc",
+  authDomain: "msqaedu-cc93b.firebaseapp.com",
+  projectId: "msqaedu-cc93b",
+  storageBucket: "msqaedu-cc93b.firebasestorage.app",
+  messagingSenderId: "759791291775",
+  appId: "1:759791291775:web:049907328b8ae348839e4a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
